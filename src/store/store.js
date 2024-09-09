ea89054ec23479e00c54b05fd39cad91706c8264
Challenger <168672409+ChallengerKnight@users.noreply.github.com>
2024-09-09 16:29:29 +0200

    Revert "connect wallet"

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,6 @@ import produce from "immer";
 
 const store = create(set => ({
     socket: null,
-    walletAddress: "",
-    isWalletConnected: false,
-
-    setWalletAddress: (address) => set(produce(state => {
-        state.walletAddress = address;
-    })),
-    
-    setIsWalletConnected: (status) => set(produce(state => {
-        state.isWalletConnected = status;
-    })),
-
     updateSocket: ( val ) => set(produce(state => {
         state.socket = val;
     }))
